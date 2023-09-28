cba17ec9595a4ce1fbaa50bb793668ef70856a02
May-Hlyan-Moe <129745420+May-Hlyan-Moe@users.noreply.github.com>
2023-09-28 23:24:48 +0630

    create a restaurant

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='App'>
+        <header className="App-header">
+          <h1>BURMESE BITES</h1>
+        </header>
+        <span>
+          <i class="fa-sharp fa-solid fa-utensils"></i>
+          RESTURANT
+          <i class="fa-sharp fa-solid fa-utensils"></i>
+        </span>
+      </div>
+      <Menu />
     </div>
   );
 }
---
+++ src/Menu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './Menu.css';
+
+export default function Menu() {
+    return (
+        <div>
+            <div className="Menu">
+                <h1>
+                📋 Menu 📋
+                </h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <h5>Mohinga</h5>
+                            <img src="https://images.squarespace-cdn.com/content/v1/5ce47ab5f347bf000199a74c/1595347809421-IPCEJCRVGVSABNHHKPVC/Store-Cupboard+Mohinga+MiMi+Aye+%285%29.jpg?format=1500w" width='200' />
+                        </div>
+                        <div className="col-6">
+                            <h5>Mohinga</h5>
+                            <img src="https://images.squarespace-cdn.com/content/v1/5ce47ab5f347bf000199a74c/1595347809421-IPCEJCRVGVSABNHHKPVC/Store-Cupboard+Mohinga+MiMi+Aye+%285%29.jpg?format=1500w" width='200' />
+                        </div>
+                        <div className="col-6">
+                            <h5>Mohinga</h5>
+                            <img src="https://images.squarespace-cdn.com/content/v1/5ce47ab5f347bf000199a74c/1595347809421-IPCEJCRVGVSABNHHKPVC/Store-Cupboard+Mohinga+MiMi+Aye+%285%29.jpg?format=1500w" width='200' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
